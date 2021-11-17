0856d7751e63a4fbff7c1f6942c7dceb4dcfbc6f
eusouocristian <eusouocristian@hotmail.com>
2021-11-17 17:32:37 -0300

    organized files

--- datetime_teste.py
+++
@@ -1,7 +0,0 @@
-from datetime import date, datetime
-
-texto = "16:15"
-dt = datetime.strptime(texto, "%H:%M")
-
-if dt.hour == datetime.now().hour and dt.minute == datetime.now().minute:
-    print("CKECHED")
